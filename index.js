d9029a8ae098ce24553950b47ae4bcc6c7b04cc3
Ajil Pappachan <ajilpappachan@gmail.com>
2021-12-17 17:51:36 +0530

    Created Ping command

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const client = new DiscordJS.Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+const WOKCommands = require("wokcommands");
+const path = require("path");
+
 client.on("ready", () => {
 	console.log("Bot is ready!");
+
+	new WOKCommands(client, {
+		commandsDir: path.join(__dirname, "commands"),
+		testServers: ["910970317160189962"],
+	});
 });
 
 client.login(process.env.TOKEN);
